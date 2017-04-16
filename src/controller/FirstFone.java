a134214bca050a46ccab5134f7382e7a63eacb64
Kazachenko1998 <kazachenko.1998k@gmail.com>
2017-04-16 16:35:48 +0300

    0.0.1

--- src/controller/FirstFone.java
+++ src/controller/FirstFone.java
@@ -11,34 +11,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
-/**
- * @author otp
- */
+
 public class FirstFone {
 //должен загрузить Загрузку и музыку
 
 
-    @FXML
-    private Pane label;
-
-    static void move(ImageView imv) {
-        AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                imv.setTranslateX(imv.getTranslateX() + 1);
-            }
-        };
-        timer.start();
-
-    }
-    @FXML
-    private void selectFile() throws InterruptedException {
-        File file = new File("ger3g.png");
-        Image im = new Image(String.valueOf(file.toURI()));
-        ImageView imv = new ImageView(im);
-        label.getChildren().add(imv);
-        move(imv);
-
-    }
 
 }
--- src/controller/SecondFone.java
+++ src/controller/SecondFone.java
@@ -4,5 +4,6 @@ package controller;
  * Created by Konstantin on 16.04.2017.
  */
 public class SecondFone {
+//загрузка всех файлов для выбора и т.д. должна быть загрузка
 
 }
