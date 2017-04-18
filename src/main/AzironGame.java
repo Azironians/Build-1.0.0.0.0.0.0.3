a64330f91573d50bd9799007774430d283afd13c
K_Konstantin_A <kazachenko.1998k@gmail.com>
2017-04-19 00:19:07 +0300

    Update AzironGame.java

--- src/main/AzironGame.java
+++ src/main/AzironGame.java
@@ -4,7 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 
 public class AzironGame extends Application {
@@ -14,10 +19,17 @@ public class AzironGame extends Application {
 
     @Override
     public void start(Stage AzironStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("../fxmlFiles/WindowInitialization.fxml"));
+        Pane root = FXMLLoader.load(getClass().getResource("../fxmlFiles/WindowInitialization.fxml"));
         AzironStage.setTitle("Heroes of Azironian");
         Scene scene = new Scene(root, 1280, 720);
-
+        File file = new File("Picture\\Windows\\WindowInitialization.jpg");
+        Image image = new Image(file.toURI().toString(),1280.0,720.0,true,true);
+        ImageView imageView = new ImageView(image);
+        File file2 = new File("Picture\\Windows\\WindowInit.bmp");
+        Image image2 = new Image(file2.toURI().toString(),1280.0,720.0,true,true);
+        ImageView imageView2 = new ImageView(image2);
+        imageView2.setFitWidth(1280.0);
+        root.getChildren().addAll(imageView,imageView2);
         AzironStage.setScene(scene);
         AzironStage.show();
         this.azironStage = AzironStage;
